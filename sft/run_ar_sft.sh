e234a53c072ca5567c1389b9c6955e2c04676722
baohaoliao <liaobaohao@gmail.com>
2025-11-13 18:03:52 +0100

    sft qwen3

---
+++ sft/run_ar_sft.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+torchrun \
+    --nnodes 1 \
+    --node_rank 0 \
+    --nproc_per_node 2 \
+    --master_addr 127.0.0.1 \
+    --master_port 12345 \
+    ./src/llamafactory/v1/launcher.py \
+    ./examples/train_full/qwen3_full.yaml
