8c6ee94ccffca3bbf4fd3c927c3f7bcf99afb20e
baohaoliao <liaobaohao@gmail.com>
2025-11-15 16:26:26 +0100

    parallel

---
+++ scripts/compute_ppl_parallel.sh
@@ -0,0 +1,29 @@
+#!/bin/bash
+
+MODEL_NAME=JetLM/SDAR-1.7B-Chat
+MODEL=/mnt/nushare2/data/baliao/PLLMs/${MODEL_NAME}
+DATA=/mnt/nushare2/data/baliao/dlm/data/hendrydong/test.json
+SAVE_DIR=/mnt/nushare2/data/baliao/dlm/00_start/ppl_results/${MODEL_NAME}
+
+mkdir -p ${SAVE_DIR}
+
+GPUS=(0 1)
+
+for block_len in 4; do
+    echo "Evaluating block_length=${block_len}"
+
+    for ((i=0; i<${#GPUS[@]}; i++)); do
+        CUDA_VISIBLE_DEVICES=${GPUS[i]} python eval/sdar_ppl_parallel.py \
+            --model_name_or_path ${MODEL} \
+            --mask_token_id 151669 \
+            --dataset_path ${DATA} \
+            --max_samples 128 \
+            --max_length 2048 \
+            --block_length ${block_len} \
+            --batch_size 64 \
+            --save_decode_orders \
+            --output_dir ${SAVE_DIR} \
+            --world_size ${#GPUS[@]} \
+            --local_idx ${i} \
+            --checkpoint_interval 20 &
+done
